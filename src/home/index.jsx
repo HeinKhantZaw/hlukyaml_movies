a51ffe665f46f0d1a7f3415f81ad412ed2bffcdf
heinkhantzaw <heinkhantzaw1@gmail.com>
2021-10-03 21:06:41 +0630

    change some texts

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -4,7 +4,7 @@ import Base from "../base";
 export default class Home extends Component {
   state = {
     link: "https://hlukyamel.pythonanywhere.com/api/movieSearch/?ordering=-id",
-    title: "Book List(Most Recent)",
+    title: "Movie List(Most Recent)",
   };
   render() {
     return (
