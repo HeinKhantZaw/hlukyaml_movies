942642b73e79cf67809a63ba3dd9c74654ebfd69
heinkhantzaw <heinkhantzaw1@gmail.com>
2021-11-01 18:12:26 +0630

    update meta tags

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -31,7 +31,7 @@ const Header = () => {
             </li>
             <li>
               <Button depressed size='large'>
-                <a target='_blank' href='https://l.facebook.com/l.php?u=https%3A%2F%2Fdocs.google.com%2Fforms%2Fd%2Fe%2F1FAIpQLScqieWpOAfJVbsEeavJ7ulJiS2ZLUomPcPWwaiyDBF3PhsnDw%2Fviewform%3Fusp%3Dsf_link%26fbclid%3DIwAR2KKfBfV8UqvLePjcrloG8YOQnqrdEzTmlPzDUeHMo_mWp94wXscVmzkoQ&h=AT0BnWitxXAbjPvIackaxV32YaVCMQCFc9IYU1Exlzvi_kER8pvtlQf3N9a82wNXf8nBFEbpG2uDREpXxQI9Yo4xwAJIocAkGl1V-a9-VIauYkVJAWBR28KKVTQCevEncb1r&__tn__=-UK-R&c[0]=AT0nzalFmAjBWuiIDFn7j1rEUlUYy7lEmmNlxsnJS4TjxLfO8owPCDGEeeP358iI5Q2xatHe4vyxaBRPS0KJVTMbVWmmXD6ia4gtwQxd-GtfZ5ftlwQQiYpmM_7FPpwxJuLve_N2U82YK58CW15uwq6kKte25cwK56n6HtGro1MDwg'>
+                <a rel={"noreferrer"} target={"_blank"} href={"https://docs.google.com/forms/d/e/1FAIpQLScqieWpOAfJVbsEeavJ7ulJiS2ZLUomPcPWwaiyDBF3PhsnDw/viewform"}>
                   Request Movies
                 </a>
               </Button>
